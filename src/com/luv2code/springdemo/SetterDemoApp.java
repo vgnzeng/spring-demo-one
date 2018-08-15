c5e40ca3aaf8c2fb3466aed1758d24adf40d29c5
Guannian Zeng <vgnzeng@gmail.com>
2018-08-14 19:54:49 -0700

    testing out the Injection of Literal Values

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -18,6 +18,10 @@ public class SetterDemoApp {
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println(theCoach.getTeam());
+		
+		System.out.println(theCoach.getEmailAddress());
 						
 		// close the context
 		context.close();
