98ebd53201b4f563e522f99b9dc58cf981ce403c
Guannian Zeng <vgnzeng@gmail.com>
2018-08-23 08:37:25 -0700

    added Method on AccountDAO for @After running demo

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.luv2code.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.luv2code.aopdemo.Account;
@@ -9,6 +12,28 @@ public class AccountDAO {
 
 	private String name;
 	private String serviceCode;
+			
+	// add a new method: findAccounts()
+	
+	public List<Account> findAccounts() {
+		
+		List<Account> myAccounts = new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		// add them to our accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		
+		return myAccounts;		
+	}
+	
+	
 	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
